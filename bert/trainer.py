345e2115e6b5ec241435f177bcda13e90ff75874
mikhailkravets <mike.k@coaxsoft.com>
2022-01-15 17:21:45 +0200

    Adjust trainer module

--- bert/trainer.py
+++ bert/trainer.py
@@ -33,7 +33,9 @@ def token_accuracy(result: torch.Tensor, target: torch.Tensor, mask: torch.Tenso
 
 class BertTrainer:
 
-    def __init__(self, model: BERT, dataset: IMDBBertDataset,
+    def __init__(self,
+                 model: BERT,
+                 dataset: IMDBBertDataset,
                  log_dir: Path,
                  checkpoint_dir: Path = None,
                  print_progress_every: int = 10,
@@ -46,19 +48,17 @@ class BertTrainer:
         self.dataset = dataset
 
         self.batch_size = batch_size
+        self.epochs = epochs
+        self.current_epoch = 0
+
         self.loader = DataLoader(self.dataset, batch_size=self.batch_size, shuffle=True)
 
         self.writer = SummaryWriter(str(log_dir))
-
         self.checkpoint_dir = checkpoint_dir
 
         self.criterion = nn.BCEWithLogitsLoss().to(device)
         self.ml_criterion = nn.NLLLoss(ignore_index=0).to(device)
         self.optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.015)
-
-        self.epochs = epochs
-
-        self.current_epoch = 0
 
         self._splitter_size = 35
 
@@ -95,17 +95,16 @@ class BertTrainer:
         average_mlm_loss = 0
         for i, value in enumerate(self.loader):
             index = i + 1
-
+            inp, mask, inverse_token_mask, token_target, nsp_target = value
             self.optimizer.zero_grad()
 
-            inp, mask, token_mask, mask_target, nsp_target = value
             token, nsp = self.model(inp, mask)
 
-            tm = token_mask.unsqueeze(-1).expand_as(token)
+            tm = inverse_token_mask.unsqueeze(-1).expand_as(token)
             token = token.masked_fill(tm, 0)
 
-            loss_token = self.ml_criterion(token.transpose(1, 2), mask_target)  # 1D tensor as target is required
-            loss_nsp = self.criterion(nsp, nsp_target)  # 1D tensor as target is required
+            loss_token = self.ml_criterion(token.transpose(1, 2), token_target)  # 1D tensor as target is required
+            loss_nsp = self.criterion(nsp, nsp_target)
 
             loss = loss_token + loss_nsp
             average_nsp_loss += loss_nsp
@@ -116,32 +115,41 @@ class BertTrainer:
 
             if index % self._print_every == 0:
                 elapsed = time.gmtime(time.time() - prev)
-                passed = percentage(self.batch_size, self._ds_len, index)
-                global_step = epoch * len(self.loader) + index
-
-                print_nsp_loss = average_nsp_loss / self._print_every
-                print_mlm_loss = average_mlm_loss / self._print_every
-
-                s = f"{time.strftime('%H:%M:%S', elapsed)}"
-                s += f" | Epoch {epoch + 1} | {index} / {self._batched_len} ({passed}%) | " \
-                     f"NSP loss {print_nsp_loss:6.2f} | MLM loss {print_mlm_loss:6.2f}"
+                s = self.training_summary(elapsed, index, average_nsp_loss, average_mlm_loss)
 
                 if index % self._accuracy_every == 0:
-                    nsp_acc = nsp_accuracy(nsp, nsp_target)
-                    token_acc = token_accuracy(token, mask_target, token_mask)
-                    s += f" | NSP accuracy {nsp_acc} | " \
-                         f"Token accuracy {token_acc}"
-                    self.writer.add_scalar("NSP train accuracy", nsp_acc, global_step=global_step)
-                    self.writer.add_scalar("Token train accuracy", token_acc, global_step=global_step)
+                    s += self.accuracy_summary(index, token, nsp, token_target, nsp_target)
 
                 print(s)
-
-                self.writer.add_scalar("NSP loss", print_nsp_loss, global_step=global_step)
-                self.writer.add_scalar("MLM loss", print_mlm_loss, global_step=global_step)
 
                 average_nsp_loss = 0
                 average_mlm_loss = 0
         return loss
+
+    def training_summary(self, elapsed, index, average_nsp_loss, average_mlm_loss):
+        passed = percentage(self.batch_size, self._ds_len, index)
+        global_step = self.current_epoch * len(self.loader) + index
+
+        print_nsp_loss = average_nsp_loss / self._print_every
+        print_mlm_loss = average_mlm_loss / self._print_every
+
+        s = f"{time.strftime('%H:%M:%S', elapsed)}"
+        s += f" | Epoch {self.current_epoch + 1} | {index} / {self._batched_len} ({passed}%) | " \
+             f"NSP loss {print_nsp_loss:6.2f} | MLM loss {print_mlm_loss:6.2f}"
+
+        self.writer.add_scalar("NSP loss", print_nsp_loss, global_step=global_step)
+        self.writer.add_scalar("MLM loss", print_mlm_loss, global_step=global_step)
+        return s
+
+    def accuracy_summary(self, index, token, nsp, token_target, nsp_target, inverse_token_mask):
+        global_step = self.current_epoch * len(self.loader) + index
+        nsp_acc = nsp_accuracy(nsp, nsp_target)
+        token_acc = token_accuracy(token, token_target, inverse_token_mask)
+
+        self.writer.add_scalar("NSP train accuracy", nsp_acc, global_step=global_step)
+        self.writer.add_scalar("Token train accuracy", token_acc, global_step=global_step)
+
+        return f" | NSP accuracy {nsp_acc} | Token accuracy {token_acc}"
 
     def save_checkpoint(self, epoch, step, loss):
         if not self.checkpoint_dir:
